daace53449759c25a5431731826df5932119dc51
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-07-13 16:10:12 +0300

    responsive

--- src/components/ContentCard.js
+++ src/components/ContentCard.js
@@ -14,25 +14,29 @@ const ContentCard = () => {
             <div className="content__col__img">
               <img src={Bestseller1} alt="" />
             </div>
-            <div className="content__col__text">
-              <p>From</p>
-              <span>$ 199.00</span>
+            <div className="content__col__wrapper">
+              <div className="content__col__wrapper__text">
+                <p>From</p>
+                <span>$ 199.00</span>
+              </div>
+              <span>A-T Steeler Front Shocks wSingle-Rate Spring</span>
+              <p>So, if you like listening to the sound of your engin...</p>
+              <Button btn="Details" />
             </div>
-            <span>A-T Steeler Front Shocks wSingle-Rate Spring</span>
-            <p>So, if you like listening to the sound of your engin...</p>
-            <Button btn="Details" />
           </div>
           <div className="content__col">
             <div className="content__col__img">
               <img src={Bestseller2} alt="" />
             </div>
-            <div className="content__col__text">
-              <p>From</p>
-              <span>60.00</span>
+            <div className="content__col__wrapper">
+              <div className="content__col__wrapper__text">
+                <p>From</p>
+                <span>60.00</span>
+              </div>
+              <span>Acura RSX Type-S HKS Hi-Power Exhaust</span>
+              <p>Even if you don't like speed and power, racing and a...</p>
+              <Button btn="Details" />
             </div>
-            <span>Acura RSX Type-S HKS Hi-Power Exhaust</span>
-            <p>Even if you don't like speed and power, racing and a...</p>
-            <Button btn="Details" />
           </div>
         </div>
       </div>
