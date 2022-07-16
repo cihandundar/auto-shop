b5d3699efcec72b153c6e1844e1cccd2dddf8ebd
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-07-16 16:03:26 +0300

    blog page update

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,17 @@
 import React from "react";
-
+import Navigation from "../components/Navigation";
+import PageTitle from "../components/PageTitle";
+import Return from "../components/Return";
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="main">
+      <Navigation />
+      <div className="main__content">
+        <Return page_name="News" />
+        <PageTitle page_title="News" />
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
