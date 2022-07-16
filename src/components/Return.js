bc0ccbcca916dff6672f95a3066012ec7cbb3623
cihandundar <94462152+cihandundar@users.noreply.github.com>
2022-07-16 14:18:10 +0300

    return component

--- src/components/Return.js
+++ src/components/Return.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Return = (props) => {
-  return <div className="return"></div>;
+  return (
+    <div className="return">
+      <div className="return__container">
+        <ul className="return__item">
+          <li className="return__link">
+            <Link>Home</Link>
+          </li>
+        </ul>
+        <span>/</span>
+        <p>{props.page_name}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Return;
